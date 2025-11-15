ad91fe0223f97527affceca736e00a718a3658be
Sourav Saha <saha83787@gmail.com>
2025-11-15 21:49:26 +0530

    sentiment analysis process updated to previous method

--- services/recommend_movies.py
+++ services/recommend_movies.py
@@ -1,17 +1,13 @@
 import os
+from transformers import pipeline
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
-from huggingface_hub import InferenceClient
 
 CUSTOM_CACHE_DIR = r"D:\HuggingFace_Cache"
 
 os.environ['HF_HOME'] = CUSTOM_CACHE_DIR
-
-api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-
-client = InferenceClient(api_key=api_token)
 
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 
@@ -20,6 +16,8 @@ movies_dataset_path = 'datasets/movies_dataset.csv'
 
 movie_embeddings = np.load(movie_embeddings_path)
 movies = pd.read_csv(movies_dataset_path)
+
+pipe = pipeline("text-classification",model='tabularisai/multilingual-sentiment-analysis')
 
 def truncate_text(text, max_chars=1800):
     return text[:max_chars]
@@ -33,11 +31,7 @@ def recommend(query):
     return {'movies':movie_results,'ids':movie_ids}
 
 def get_positive_score(movie):
-    positive_scores = [
-        s.score
-        for s in movie['sentiment_scores']
-        if s.label in ['Very Positive', 'Positive']
-    ]
+    positive_scores = [s['score'] for s in movie['sentiment_scores'] if s['label'] in ['Very Positive','Positive']]
 
     if not positive_scores:
         return 0
@@ -52,20 +46,18 @@ def recommend_based_on_sentiment(reviews,movies):
     }
 
     for movie_id,review in cleaned_reviews.items():
-        shorter_reviews = [truncate_text(r) for r in review]
+        # shorter_reviews = [truncate_text(r) for r in review]
+        print("model called")
         try:
-            model_response = client.text_classification(
-                shorter_reviews,
-                model="tabularisai/multilingual-sentiment-analysis"
-            )
+            model_response = pipe(review,truncation=True)
+            print("Successfully generated")
             all_movie_sentiments.append({
                 'movie_id':movie_id,
                 'sentiment_scores':model_response
             })
+            print(all_movie_sentiments)
         except Exception as e:
             print(f"\nError fetching data for : {e}")
-
-    print("Sentiment Analysis done")
     
     for movie in all_movie_sentiments:
         movies['score'] = get_positive_score(movie)
